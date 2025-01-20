296fa8390a3b4bcfe78faf2fcd4d8584ced1c8d9
Unleashed01 <b421014@iiit-bh.ac.in>
2025-01-20 20:51:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Body from './components/Body/Body'
 import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
